3dfcc9295257f0262618a775706830a3cfb184bf
FireLogin <reactnativeamalgum@gmail.com>
2023-06-23 13:23:20 +0530

    DEV-14 Forgot Password UI under process

--- Components/Routes/Route.js
+++ Components/Routes/Route.js
@@ -1,22 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native'
-import navigationStrings from '../Navigation/NavigationStrings/navigationStrings'
-import { ForgotPass, Login, SignUp } from '../../Screens'
-
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import navigationStrings from '../Navigation/NavigationStrings/navigationStrings';
+import {ForgotPass, Login, SignUp} from '../../Screens';
 
 const Stack = createNativeStackNavigator();
 
 export default function Route() {
   return (
-    <NavigationContainer >
-        <Stack.Navigator screenOptions={{headerShown:false}} >
-        <Stack.Screen name ={navigationStrings.SIGNUP} component={SignUp} />
-            <Stack.Screen name= {navigationStrings.LOGIN} component={Login} />
-            <Stack.Screen name= {navigationStrings.FORGOTPASSWORD} component={ForgotPass} />
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
 
-            </Stack.Navigator>
+        >
+        <Stack.Screen name={navigationStrings.SIGNUP} component={SignUp} />
+        <Stack.Screen name={navigationStrings.LOGIN} component={Login} />
+        <Stack.Screen
+          name={navigationStrings.FORGOTPASSWORD}
+          component={ForgotPass}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
