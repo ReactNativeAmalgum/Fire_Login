277e337a829d90ca5558e8702fa8dd2f27ba7ea9
FireLogin <reactnativeamalgum@gmail.com>
2023-06-24 14:09:20 +0530

    DEV-16 First splahScreen commit

--- Components/Navigation/NavigationStrings/navigationStrings.js
+++ Components/Navigation/NavigationStrings/navigationStrings.js
@@ -2,5 +2,6 @@ export default {
     'SIGNUP': 'SignUp',
     'LOGIN':'Login',
     'FORGOTPASSWORD':'ForgotPass',
-    'WELCOME':'Welcome'
+    'WELCOME':'Welcome',
+    "SPLASHSCREEN":'SplashScreen'
 }
